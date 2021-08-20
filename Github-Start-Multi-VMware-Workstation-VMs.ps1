a7f35de6ea669ea14a5cbd71aeb2b4521c1e7419
samverse <sam.adane@yahoo.com>
2021-08-20 15:55:05 -0400

    Add files via upload

---
+++ Github-Start-Multi-VMware-Workstation-VMs.ps1
@@ -0,0 +1,16 @@
+﻿Function Start-VMsAuto {
+
+$VMRun = "C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe"
+
+#Input VMs to autostart on a new line separated by comma;
+$Auto_Start_VMs = @(
+"D:\Home-Lab\VMware\DC01.vmx",
+"D:\Home-Lab\VMware\SCCM.vmx",
+"D:\Home-Lab\VMware\WSUS.vmx"
+)
+
+$Auto_Start_VMs | ForEach-Object {& $VMRun -T ws start $($_)}
+
+}
+
+Start-VMsAuto
